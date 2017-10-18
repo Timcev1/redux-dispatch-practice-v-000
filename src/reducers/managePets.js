90f9fe48670bf71ef9981680ab1d07d253344ba0
Timothy Cevallos <timcev@gmail.com>
2017-10-18 01:43:56 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -10,5 +10,9 @@ export function dispatch(){
 };
 
 export function render(){
-
+  let container = document.getElementById('container');
+  let petsList = state.pets.map((pet) => {
+    return `<li>${pet.name}</li>`;
+  }).join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`;
 }
